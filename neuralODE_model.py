1. renamed model| 2. fixed issues with metrics calculation

neuralODE_model.py
@@ -13,7 +13,7 @@ class KCONDE_FCC(nn.Module):
             nn.Linear(hidden_size, n_reactions),
             nn.Softplus()  # положительные скорости
         )
-        self.beta = nn.Parameter(torch.tensor(0.05), requires_grad=True)  # теперь обучаемый
+        self.beta = nn.Parameter(torch.tensor(0.05), requires_grad=True)
 
     def predict_rates(self, y, C_O, T):
         # y: (batch, 5), C_O и T: скаляры или тензоры (batch,1)
@@ -41,14 +41,14 @@ class KCONDE_FCC(nn.Module):
         def ode_func(t, y):
             # y: (batch, 5)
             rates = self.predict_rates(y, self._C_O, self._T)  # (batch,6)
-            v1,v2,v3,v4,v5,v6 = rates.unbind(dim=1)
+            k1,k2,k3,k4,k5,k6 = rates.unbind(dim=1)
             phi = torch.exp(-self.beta * t)
             C_VGO, C_LCO, C_gas, C_light, C_coke = y.unbind(dim=1)
-            dVGO = -(v1+v2+v3+v4) * (C_VGO**2) * phi
-            dLCO = (v1 * (C_VGO**2) - v5 * C_LCO) * phi
-            dGas = (v2 * (C_VGO**2) + v5 * C_LCO - v6 * C_gas) * phi
-            dLight = (v3 * (C_VGO**2) + v6 * C_gas) * phi
-            dCoke = (v4 * (C_VGO**2)) * phi
+            dVGO = -(k1+k2+k3+k4) * (C_VGO**2) * phi
+            dLCO = (k1 * (C_VGO**2) - k5 * C_LCO) * phi
+            dGas = (k2 * (C_VGO**2) + k5 * C_LCO - k6 * C_gas) * phi
+            dLight = (k3 * (C_VGO**2) + k6 * C_gas) * phi
+            dCoke = (k4 * (C_VGO**2)) * phi
             return torch.stack([dVGO, dLCO, dGas, dLight, dCoke], dim=1)
         
         max_t = t_final_batch.max().item()

plot.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if args.model == 'flexible':
-    from kconde_model import KCONDE_FCC
+    from neuralODE_model import KCONDE_FCC
     model = KCONDE_FCC().to(device)
     checkpoint = args.checkpoint if args.checkpoint else 'kconde_flexible.pth'
     normalize_T = True

train.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 
 if args.model == 'flexible':
-    from kconde_model import KCONDE_FCC
+    from neuralODE_model import KCONDE_FCC
     model_class = KCONDE_FCC
     hidden_size = args.hidden if args.hidden is not None else 128
     normalize_T = True   # в flexible модели T нормируется ( (T-500)/100 )
@@ -111,7 +111,7 @@ for epoch in range(args.epochs):
         with torch.no_grad():
             y_pred_val = model.integrate_batch(C0_val, t_s_val, C_O_val, T_val)
             mse_val, rmse_pct, mae_pct = compute_metrics(y_pred_val, targets_val)
-            print(f"Epoch {epoch+1:4d}/{args.epchs}, Train Loss: {loss_train.item():.6f} |"
+            print(f"Epoch {epoch+1:4d}/{args.epochs}, Train Loss: {loss_train.item():.6f} |"
                   f"Val Loss: {mse_val:.6f} | Val RMSE(%) : {rmse_pct:.2f} | Val MAE(%) : {mae_pct:.2f}")
 
 torch.save(model.state_dict(), f'kconde_{args.model}_final.pth')

utils.py
@@ -18,9 +18,10 @@ def rmse(y_true, y_pred):
 # Function for validation (val loss, MSE, MAE)
 def compute_metrics(y_pred, y_true):
     with torch.no_grad():
-        mse = nn.MSELoss(y_pred, y_true)
+        loss_fn = nn.MSELoss()
+        mse = loss_fn(y_pred, y_true)
         y_pred_pct = y_pred * 100.0
         y_true_pct = y_true * 100.0
-        rmse_pct = rmse(y_true_pct, y_pred_pct)
+        rmse_pct = rmse(y_true_pct.cpu().numpy(), y_pred_pct.cpu().numpy())
         mae_pct = torch.mean(torch.abs(y_pred_pct - y_true_pct))
     return mse.item(), rmse_pct.item(), mae_pct.item()